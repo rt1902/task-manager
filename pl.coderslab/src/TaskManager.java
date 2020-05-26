9959af9a3f9ab5fbf1d70623fe82b662d2c8897b
rt1902 <robert.tomalaaa@gmail.com>
2020-05-26 12:23:07 +0200

    Add menu listing metod

--- pl.coderslab/src/TaskManager.java
+++ pl.coderslab/src/TaskManager.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class TaskManager {
 
     public static void main(String[] args) {
 
+        menu();
+
+    }
+    private static void menu() {
+        String[] menuArray = {"add", "remove", "list", "exit"};
+        System.out.println(ConsoleColors.BLUE + "Please select an option");
+        for (int i = 0; i < menuArray.length; i++) {
+            System.out.println(ConsoleColors.WHITE + menuArray[i]);
+        }
     }
 }
